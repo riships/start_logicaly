3697320cd19bf20cc7f88946b44f84649a1d7043
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2023-12-13 14:02:58 +0000

    operators and operations

---
+++ NewCodingWithchaiOrCode/operations.js
@@ -0,0 +1,9 @@
+console.log(1 + 2 + 3);
+
+
+// it will convert the rest in string as per the first parameter
+console.log("1" + 2 + 3);
+
+
+// first two will be added and the rest will be coverted as number
+console.log(1 + 2 + "3" + 3); 
---
+++ NewCodingWithchaiOrCode/operators.js
@@ -0,0 +1,28 @@
+// lets talk about increment operator
+
+
+// 1. its a prefix operator
+let x = 4;
+let y = ++x;
+
+// console.log(y);
+// console.log(x);
+
+// 2.Postfix operator
+let a = 5;
+
+let b = a++;
+
+console.log(b);
+console.log(a);
+
+
+// there is the same operator in js as decrement operator u can use -- to this operator
+
+// Example : - 
+// a-- this is post fix
+// --a this is prefix 
+
+
+// these are the docs created by 
+
