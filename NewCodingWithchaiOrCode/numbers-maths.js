f67d871f4d40a3eb162deaa99245a419baf674a7
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2023-12-15 15:23:59 +0000

    number and math tutorials

---
+++ NewCodingWithchaiOrCode/numbers-maths.js
@@ -0,0 +1,31 @@
+let myNumVal = 1258565;
+
+
+const myBal = new Number(100);
+
+// console.log(myBal);
+
+
+const myNewVal = 100.555;
+
+// console.log(myNewVal.toFixed(2)); //it fix the value after . and get the roundoff value
+//console.log(myNewVal.toPrecision(4)); //it fix the number value with roundoff
+
+
+
+// ================== Lets see math ==============
+
+console.log(Math);
+
+
+console.log((Math.random() * 10) + 1);
+
+console.log(Math.round(Math.random() * 10) + 1);
+
+
+const maxVal = 20;
+const minVal = 10;
+
+
+console.log(Math.floor(Math.random() * (maxVal - minVal)) + minVal);
+
--- NewCodingWithchaiOrCode/strings.js
+++ NewCodingWithchaiOrCode/strings.js
@@ -1,1 +1,31 @@
-// 
+// this is string method let's try and see the output 
+
+let myNameWithAddress = 'rishi-prakash-soni-nitishhomesnoida';
+
+// console.log(myNameWithAddress);
+
+// now lets see the method to get index of any string
+let findIndex = myNameWithAddress.indexOf('i');
+let findTheValue = myNameWithAddress.charAt(5);
+
+// let countingOfChar = 0;
+// for (let i = 0; i < myNameWithAddress.length; i++) {
+//     if (myNameWithAddress[i] == 'r') { 
+//         countingOfChar++ 
+//     }
+// };
+// console.log(countingOfChar); it will print the all counting of r
+
+
+
+
+// console.log(findIndex);
+console.log(myNameWithAddress.__proto__);
+
+// document.body.innerHTML = myNameWithAddress.at();
+// let myNewColordVal = myNameWithAddress.replaceAll('-', '<br>');
+let myNewConstructVal = new String(myNameWithAddress);
+
+// document.write(myNewColordVal)
+// console.log(myNewConstructVal.length);
+
